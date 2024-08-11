31a70c82ab79b5b24f18af129adfb6ba64efe388
Cagri <cagriorskaya@hotmail.com>
2024-08-11 18:43:34 +0300

    Add styles to not found page

--- src/pages/404/Index.tsx
+++ src/pages/404/Index.tsx
@@ -1,14 +1,37 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
 const NotFound: React.FC = () => {
   return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <h1 className={styles.statusCode}>404</h1>
+        <h3 className={styles.heading}>Oops! Page Not Found</h3>
+        <p className={styles.explanation}>
+          The page you are looking for might have been removed, had its name
+          changed, or is temporarily unavailable.
+        </p>
+        <span className={styles.listHeading}>What can you do?</span>
+        <ul>
+          <li>Double-check the URL for typos.</li>
+          <li>
+            Return to the{" "}
+            <Link className={styles.link} to="">
+              homepage
+            </Link>
+            .
+          </li>
+          <li>
+            If you believe this is an error, please{" "}
+            <Link className={styles.link} to="#">
+              contact us
+            </Link>
+            .
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default NotFound;
